84dfd3599af3c6d9dcbd2523aefca803e336fe6e
JarbiBonilla <jarbibonilla@gmail.com>
2020-06-28 21:19:05 -0400

    created a class constructor for my anime.js frontend

--- js/models/anime.js
+++ js/models/anime.js
@@ -1,7 +1,10 @@
 class Anime {
-
-
-
-
-    
+    constructor(id, title, genre, summary, rating, favorite, image)
+    this.id = id
+    this.title = title
+    this.genre = genre
+    this.summary = summary
+    this.rating = rating
+    this.favorite = favorite
+    this.image = image 
 }
