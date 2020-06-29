af41aaabca4ad9d6beabdbac4e5e18581c4389ed
JarbiBonilla <jarbibonilla@gmail.com>
2020-06-29 02:48:15 -0400

    forgot to add () to API.addAnimes in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    API.addAnimes
+    API.addAnimes()
     // add the form event listener to the domContentLoaded
     document.getElementById('form').addEventListener('submit', API.addAnime)
 })
