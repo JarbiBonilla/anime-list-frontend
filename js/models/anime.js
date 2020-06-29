f291121818f0e6610bacfcac1866d90211a0b768
JarbiBonilla <jarbibonilla@gmail.com>
2020-06-29 00:41:33 -0400

    created an api.js file, anime.js, index.js, style.css, and index.html

--- js/models/anime.js
+++ js/models/anime.js
@@ -1,10 +1,16 @@
 class Anime {
-    constructor(id, title, genre, summary, rating, favorite, image)
-    this.id = id
-    this.title = title
-    this.genre = genre
-    this.summary = summary
-    this.rating = rating
-    this.favorite = favorite
-    this.image = image 
+    constructor(id, title, genre, summary, rating, favorite, image){
+        this.id = id
+        this.title = title
+        this.genre = genre
+        this.summary = summary
+        this.rating = rating
+        this.favorite = favorite
+        this.image = image 
+    }
+    
+    renderAnime() {
+
+
+    }
 }
