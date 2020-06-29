7dd9d7b36571fc9806b0ed290b100becd7a531d1
JarbiBonilla <jarbibonilla@gmail.com>
2020-06-29 01:00:19 -0400

    added api functions to index.js event listener

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
+    API.addAnimes
+    // add the form event listener to the domContentLoaded
+    document.getElementById('form').addEventListener('submit', API.addAnime)
 })
